51d4c26001a2a9ddeea292040d4401b7c13b99b2
rhithesh <rhithesh1947@gmail.com>
2025-09-01 22:51:48 +0530

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="tWiekXgAPZ1--I_4lL2ll5zlGCxjaHgM_jx3Ikistho" />
         {/* Leaflet CSS */}
         <link
           rel="stylesheet"
